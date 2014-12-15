ca61d60ca8468e7ff9a7f3df3b511a4f90aad8f0
Sankaranarayanan Viswanathan <rationalrevolt@gmail.com>
2014-12-14 21:26:03 -0500

    Correct verify and remove should-swap?

--- src/dailyprogrammer/heap.clj
+++ src/dailyprogrammer/heap.clj
@@ -1,6 +1,6 @@
 (ns dailyprogrammer.heap)
 
-(declare make-heap bubble-up bubble-down swap should-swap? parent-index child-indeces verify)
+(declare make-heap bubble-up bubble-down swap parent-index child-indeces verify)
 
 (defn make-max-heap [compare-fn]
   (make-heap compare-fn []))
@@ -21,7 +21,7 @@
         len    (count data)
         result (make-heap comp
                           (bubble-up comp (conj data item) len))]
-    #_(verify (:data result))
+    #_(verify comp (:data result))
     result))
 
 (defn heap-remove [heap]
@@ -33,7 +33,7 @@
                                           (take (dec len))
                                           (vec))]
                             (bubble-down comp data 0)))]
-    #_(verify (:data result))
+    #_(verify comp (:data result))
     result))
 
 (defn heap-seq [heap]
@@ -52,9 +52,11 @@
   (let [pindx  (parent-index indx)]
     (if (< indx 1)
       data
-      (if (should-swap? comp data indx pindx)
-        (recur comp (swap data indx pindx) pindx)
-        data))))
+      (let [parent (nth data pindx)
+            child  (nth data indx)]
+        (if (pos? (comp child parent))
+          (recur comp (swap data indx pindx) pindx)
+          data)))))
 
 (defn- bubble-down [comp data indx]
   (let [len           (count data)
@@ -66,19 +68,18 @@
                             (if (pos? (comp c1 c2)) i1 i2))
                         1 (first child-indeces)
                         0 nil)]
-    (if (and maxc-indx (should-swap? comp data maxc-indx indx))
-      (recur comp (swap data maxc-indx indx) maxc-indx)
+    (if maxc-indx
+      (let [parent (nth data indx)
+            child  (nth data maxc-indx)]
+        (if (pos? (comp child parent))
+          (recur comp (swap data maxc-indx indx) maxc-indx)
+          data))
       data)))
 
 (defn- swap [data i1 i2]
   (assoc data
     i1 (nth data i2)
     i2 (nth data i1)))
-
-(defn- should-swap? [comp data i1 i2]
-  (let [a (nth data i1)
-        b (nth data i2)]
-    (pos? (comp a b))))
 
 (defn- parent-index [indx]
   (quot (dec indx) 2))
@@ -87,14 +88,14 @@
   (let [base (* indx 2)]
     [(+ base 1) (+ base 2)]))
 
-(defn- verify [data]
+(defn- verify [comp data]
   (let [indx (-> data count dec)]
     (loop [indx indx]
       (if (>= indx 1)
         (let [pindx (parent-index indx)
               p     (nth data pindx)
               c     (nth data indx)]
-          (if (< p c)
+          (if-not (pos? (comp p c))
             (do (println data)
                 (throw (RuntimeException. (str "Heap property unsatisfied at: " indx))))
             (recur (dec indx))))))))
